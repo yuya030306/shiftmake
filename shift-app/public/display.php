7ef29c9cde52585af76ebfce4f3080ae61c34450
Doffy <doffy0303yuya@gmail.com>
2025-01-07 01:09:52 +0900

    f

--- shift-app/public/display.php
+++ shift-app/public/display.php
@@ -5,8 +5,8 @@ if (!isset($_SESSION['loggedin']) || !$_SESSION['loggedin']) {
     exit;
 }
 
-require_once '../includes/db_connect.php';
-require_once '../includes/functions.php';
+require_once __DIR__ . '/../includes/db_connect.php';
+require_once __DIR__ '../includes/functions.php';
 
 $selected_date = isset($_GET['date']) ? $_GET['date'] : date('Y-m-d');
 
